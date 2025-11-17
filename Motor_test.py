a11c7fcf4a31dd2ad3e9971f6c1a94cd2e768b48
NOVA-DNT <novacecy5@gmail.com>
2025-11-17 14:49:08 -0600

    Primera version de Motor_test.py funcionando en main

---
+++ Motor_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import time
+from gpiozero import PWMOutputDevice
+
+ESC_PIN = 18
+esc = PWMOutputDevice(ESC_PIN, frequency=50)
+
+print("--- PRUEBA DE MOTOR ---")
+
+# Neutral
+esc.value = 0.075
+print("Motor en neutral...")
+time.sleep(2)
+
+print("Motor avanzando MEDIO...")
+esc.value = 0.089  # VELOCIDAD MEDIA
+time.sleep(3)
+
+# DETENER
+print("Motor detenido")
+esc.value = 0.075
+time.sleep(1)
+
+esc.close()
+print("Prueba finalizada")
